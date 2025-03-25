b634a52f357759b0294bbedc0e332db191d1a2ef
Amid Esfahani <amid.esfahani@gmail.com>
2025-03-25 17:58:39 +0330

    update resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -22,12 +22,12 @@ function Experience() {
 					<div className="flex flex-col lg:flex-row w-full items-start justify-start">
 						<TabList className="relative hidden lg:flex lg:w-1/5 lg:flex-col">
 							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-left text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">Zaman</Tab>
+							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-left text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">Roja</Tab>
 							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-left text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">Tadbir</Tab>
 							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-center text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">Shaparak</Tab>
 							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-center text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">iBelit</Tab>
 							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-left text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">Artimes Co</Tab>
 							<Tab className="flex h-10 w-full cursor-pointer select-none appearance-none items-center justify-start whitespace-nowrap border-l-[2px] border-gray-600 bg-transparent px-5 pb-px text-left text-gray-400 outline-none transition-all duration-200 hover:bg-gray-600/20">Silver Design</Tab>
-
 							<div style={{ transform: "translateY(calc(" + tabIndex + " * var(--tab-height)))", transition: "transform 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0.1s" }} className="tab-highlight hidden lg:block absolute top-0 left-0 z-10 h-10 w-[2px] rounded bg-primary-400"></div>
 						</TabList>
 						<div className="w-full lg:w-4/5 mt-5 lg:mt-0 flex flex-col items-start justify-start space-y-10 lg:space-y-0">
@@ -40,7 +40,7 @@ function Experience() {
 											@ <a href="https://zaman.tech/" target="_blank" rel="nofollow">Zaman</a>
 										</span>
 									</h3>
-									<p className="text-sm text-gray-500">May 2022 - Present</p>
+									<p className="text-sm text-gray-500">May 2022 - JAN 2025</p>
 									<div className="mt-6">
 										<ul className="list-none text-sm">
 											<li className="mb-3 pl-6">I work as a senior full-stack programmer in Zaman company.</li>
@@ -57,6 +57,30 @@ function Experience() {
 												projects.
 											</li>
 											<li className="mb-3 pl-6">Interfaced with clients on a weekly basis, providing technological expertise</li>
+											<li className="mb-3 pl-6">Logmaster (Australia): Enhanced backend scalability using NestJS and MongoDB, delivering a modular and maintainable architecture.</li>
+											<li className="mb-3 pl-6">YouTube Revenue Purchase System (OleFunds, UK): Designed a Laravel-based API integrating YouTube APIs to connect channels and retrieve video data. Implemented user authentication via Onfido and payment workflows with GoCardless for scheduled withdrawals. Optimized data retrieval for rapid API responses.</li>
+										</ul>
+									</div>
+								</div>
+							</TabPanel>
+
+							<TabPanel>
+								<div className="border-b border-gray-800 pb-5 lg:border-b-0 lg:pb-0">
+									<h3 className="mb-1 text-xl">
+										<span className="text-gray-200">Full Stack Developer</span>
+										<span className="text-primary-400">
+											{" "}
+											@ <a href="https://rojashop.com/" target="_blank" rel="nofollow">RojaShop</a>
+										</span>
+									</h3>
+									<p className="text-sm text-gray-500">DEC 2023 - JAN 2025</p>
+									<div className="mt-6">
+										<ul className="list-none text-sm">
+											<li className="mb-3 pl-6">Engineered and maintained high-performance e-commerce applications using Laravel, integrating MySQL and MongoDB databases for seamless data management.</li>
+											<li className="mb-3 pl-6">Designed scalable solutions in collaboration with cross-functional teams, leveraging Docker for streamlined development and deployment workflows.</li>
+											<li className="mb-3 pl-6">Boosted system performance by implementing caching strategies with Varnish Cache and Redis, reducing latency and enhancing user experience.</li>
+											<li className="mb-3 pl-6">Developed real-time, high-throughput APIs using Node.js (Fastify), ensuring efficient data processing.</li>
+											<li className="mb-3 pl-6">Implemented a virtual product testing feature using a live simulation library with camera integration, enhancing user engagement.</li>
 										</ul>
 									</div>
 								</div>
